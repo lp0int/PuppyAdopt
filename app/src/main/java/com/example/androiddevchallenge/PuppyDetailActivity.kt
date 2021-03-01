399fa27808d03d7e50333f50e11da1caf0098870
“lp0int” <xiaoawl23@hotmail.com>
2021-03-01 15:51:27 +0800

    fix ktlint

--- app/src/main/java/com/example/androiddevchallenge/PuppyDetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyDetailActivity.kt
@@ -5,10 +5,13 @@ import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.Button
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
